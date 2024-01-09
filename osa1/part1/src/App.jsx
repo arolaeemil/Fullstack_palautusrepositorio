c38a3cd5b5b0f99442e76489515f0dc3062c75d9
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-01-09 11:34:16 +0200

    parempi counter

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,33 +1,18 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
+import { useState } from 'react'
 
-  return (
-    <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born {bornYear()}</p>
-    </div>
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
   )
-}
 
-const App = (props) => {
-    const {counter} = props
+  console.log('rendering...', counter)
+
   return (
     <div>{counter}</div>
   )
 }
 
 export default App
-
-// a + c osan alun app.
-// const App = (props) => {
-//   const nimi = 'Pekka'
-//   const ika = 10
-
-//   return (
-//     <>
-//       <h1>Greetings</h1>
-//       <Hello name="Maya" age={26 + 10} />
-//       <Hello name={nimi} age={ika} />
-//     </>
-//   )
-// }
