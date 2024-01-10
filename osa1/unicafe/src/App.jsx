40bed7b90233d196cdf662ab5ff6d777394e9676
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-01-10 16:26:45 +0200

    t1.11

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -58,15 +58,29 @@ const Statistics = ({ good, neutral, bad }) =>{
     )
   }
   return(
-  <p>
-    {/* good {good}<br/> */}
-    <StatisticLine text="good" value ={good} />
-    <StatisticLine text="neutral" value ={neutral} />
-    <StatisticLine text="bad" value ={bad} />
-    <StatisticLine text="all" value ={total} />
-    <StatisticLine text="average" value ={average} />
-    <StatisticLine text="positive" value ={good/total} />
-  </p>)
+    <table>
+      <tbody>
+        <tr>
+          <td><StatisticLine text="good" value ={good} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine text="neutral" value ={neutral} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine text="bad" value ={bad} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine text="all" value ={total} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine text="average" value ={average} /></td>
+        </tr>
+        <tr>
+          <td><StatisticLine text="positive" value ={good/total} /></td>
+        </tr>
+      </tbody>
+    </table>
+    )
   }
 
 const StatisticLine = (props) =>{
