60ab062ab193459a0ad9e79cd528973ee260a2e8
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-01-14 14:54:07 +0200

    osa b valmis

--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -1,46 +1,55 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-// const App = (props) => {
-const App = ({ notes }) => {
-  // const { notes } = props
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+  const [showAll, setShowAll] = useState(true)
 
-  //siirrettiin moduuliin
-  // const Note = ({ note }) => {
-  //   return (
-  //     <li>{note.content}</li>
-  //   )
-  // }
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important === true)
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
-      </ul>
-      {/* <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
-      </ul> */}
-      {/* Tämä on paras tapa näistä */}
-      {/* <ul>
-        {notes.map(note => 
-          <li key={note.id}>
-            {note.content}
-          </li>
-        )}
-      </ul> */}
-      {/* <ul>
-        {notes.map((note, i) => 
-          <li key={i}>
-            {note.content}
-          </li>
-        )}
-      </ul> */}
+      </ul>   
+      <form onSubmit={addNote}>
+        <input value={newNote}
+        onChange={handleNoteChange}/>
+        
+        <button type="submit">save</button>
+      </form>  
     </div>
   )
 }
 
-export default App
+export default App 
