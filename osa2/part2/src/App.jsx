9c67ae58c2e22af2c5b603cdc2ab895878ad4dda
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-01-16 17:41:26 +0200

    osa e

--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -3,6 +3,21 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import noteService from './services/notes'
 
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2023</em>
+    </div>
+  )
+}
+
 const App = (props) => {
   const [notes, setNotes] = useState([])
   // const [notes, setNotes] = useState(props.notes)
@@ -10,6 +25,7 @@ const App = (props) => {
     'a new note...'
   )
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   // useEffect(() => {
   //   console.log('effect')
@@ -85,9 +101,15 @@ const App = (props) => {
     //   setNotes(notes.map(note => note.id !== id ? note : response.data))
     })
     .catch(error => {
-      alert(
-        `the note '${note.content}' was already deleted from server`
+      setErrorMessage(
+        `Note '${note.content}' was already removed from server`
       )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+      // alert(
+      //   `the note '${note.content}' was already deleted from server`
+      // )
       setNotes(notes.filter(n => n.id !== id))
     })
     // axios.put(url, changedNote).then(response => {
@@ -108,6 +130,7 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
@@ -124,6 +147,19 @@ const App = (props) => {
         
         <button type="submit">save</button>
       </form>  
+      <Footer />
+    </div>
+  )
+}
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="error">
+      {message}
     </div>
   )
 }
