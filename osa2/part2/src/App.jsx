cf029524f13fac375251cdfee36d39509a3920c9
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-01-16 14:49:34 +0200

    osa d

--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -1,6 +1,7 @@
 import Note from './components/Note'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import noteService from './services/notes'
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
@@ -20,17 +21,26 @@ const App = (props) => {
   //     })
   // }, [])
 
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }
+  // const hook = () => {
+  //   console.log('effect')
+  //   axios
+  //     .get('http://localhost:3001/notes')
+  //     .then(response => {
+  //       console.log('promise fulfilled')
+  //       setNotes(response.data)
+  //     })
+  // }
   
-  useEffect(hook, [])
+  // useEffect(hook, [])
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      // .then(response => {
+      //   setNotes(response.data)
+      })
+  }, [])
 
   console.log('render', notes.length, 'notes')
 
@@ -39,15 +49,55 @@ const App = (props) => {
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
-      id: notes.length + 1,
+      // id: notes.length + 1,
     }
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    noteService
+    .create(noteObject)
+    .then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
+    // .then(response => {
+    //   setNotes(notes.concat(response.data))
+    //   setNewNote('')
+    })
+    // axios
+    // .post('http://localhost:3001/notes', noteObject)
+    // .then(response => {
+    //   // console.log(response)
+    //   setNotes(notes.concat(response.data))
+    //   setNewNote('')
+    // })
+    // setNotes(notes.concat(noteObject))
+    // setNewNote('')
   }
 
+  const toggleImportanceOf = (id) => {
+    // console.log(`importance of ${id} needs to be toggled`)
+    // console.log('importance of ' + id + ' needs to be toggled')
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+    .update(id, changedNote)
+    .then(returnedNote => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    // .then(response => {
+    //   setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+    .catch(error => {
+      alert(
+        `the note '${note.content}' was already deleted from server`
+      )
+      setNotes(notes.filter(n => n.id !== id))
+    })
+    // axios.put(url, changedNote).then(response => {
+    //   setNotes(notes.map(note => note.id !== id ? note : response.data))
+    // })
+  }
+
+
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
@@ -65,7 +115,7 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
         )}
       </ul>   
       <form onSubmit={addNote}>
--- osa2/part2/src/components/Note.jsx
+++ osa2/part2/src/components/Note.jsx
@@ -1,7 +1,18 @@
-const Note = ({ note }) => {
-    return (
-      <li>{note.content}</li>
-    )
-  }
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important
+    ? 'make not important' : 'make important'
+
+  return (
+    <li>
+      {note.content} 
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  )
+}
+// const Note = ({ note }) => {
+//     return (
+//       <li>{note.content}</li>
+//     )
+//   }
   
   export default Note
