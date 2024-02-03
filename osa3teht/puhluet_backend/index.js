25d6361a963b817fc7d8294978457f4e01cfe3a4
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-02-03 22:03:50 +0200

    t3.8

--- osa3teht/puhluet_backend/index.js
+++ osa3teht/puhluet_backend/index.js
@@ -3,7 +3,9 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
+morgan.token('req-body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
 let persons = [
     {
