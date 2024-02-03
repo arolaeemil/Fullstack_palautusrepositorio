20bfe550bfbe2b59dcd95e32744c8201f83b7396
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-02-03 17:16:58 +0200

    osa 3a middlewaret

--- osa3/part3/index.js
+++ osa3/part3/index.js
@@ -12,7 +12,21 @@
 const express = require('express')
 const app = express()
 
+//omat middlewaret
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(express.json())
+app.use(requestLogger)
 
 let notes = [
     {
@@ -88,6 +102,8 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
