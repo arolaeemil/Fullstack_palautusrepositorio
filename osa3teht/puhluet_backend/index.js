7a0b85a2729f327a5374a655a58d770d43453d6a
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-02-06 17:56:43 +0200

    t3.11

--- osa3teht/puhluet_backend/index.js
+++ osa3teht/puhluet_backend/index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 // app.use(morgan('tiny'))
 morgan.token('req-body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
