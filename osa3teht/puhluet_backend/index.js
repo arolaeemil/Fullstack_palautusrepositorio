b64cdad5045e94d350266f91f7fb90db75d50bee
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-02-07 10:22:30 +0200

    t3.15

--- osa3teht/puhluet_backend/index.js
+++ osa3teht/puhluet_backend/index.js
@@ -36,6 +36,16 @@ let persons = [
     },
   ]
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.get('/', (request, response) => {
   response.send('<h1>Puhelinluettelo!</h1>')
 })
@@ -74,10 +84,16 @@ app.get('/api/info', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+//   const id = Number(request.params.id)
+//   persons = persons.filter(person => person.id !== id)
 
-  response.status(204).end()
+//   response.status(204).end()
+// })
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -141,6 +157,8 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
