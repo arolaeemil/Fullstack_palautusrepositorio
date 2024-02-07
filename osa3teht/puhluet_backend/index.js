5974618dde9d4f1e76e876c4853655aaade18e61
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-02-07 12:21:32 +0200

    t3.21

--- osa3teht/puhluet_backend/index.js
+++ osa3teht/puhluet_backend/index.js
@@ -9,32 +9,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
-// app.use(morgan('tiny'))
 morgan.token('req-body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
-
-// let persons = [
-//     {
-//       id: 1,
-//       name: "Arto Hellas",
-//       number: "040-123456"
-//     },
-//     {
-//       id: 2,
-//       name: "Ada Lovelace",
-//       number: "39-44-5323523"
-//     },
-//     {
-//       id: 3,
-//       name: "Dan Abramov",
-//       number: "12-43-234345"
-//     },
-//     {
-//       id: 4,
-//       name: "Mary Poppendick",
-//       number: "39-23-6423122"
-//     },
-//   ]
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
@@ -50,10 +26,6 @@ app.get('/', (request, response) => {
   response.send('<h1>Puhelinluettelo!</h1>')
 })
 
-// app.get('/api/persons', (request, response) => {
-//   response.json(persons);
-// })
-
 app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     if (persons) {
@@ -64,21 +36,8 @@ app.get('/api/persons', (request, response, next) => {
     .catch(error => next(error))
 })
 
-// app.get('/api/persons/:id', (request, response) => {
-//   const id = Number(request.params.id)
-//   const person = persons.find(person => person.id === id)
-
-//   if (person) {
-//     response.json(person)
-//   } else {
-//     response.status(404).end()
-//   }
-// })
-
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
-  //   response.json(person)
-  // })
     if (person) {
       response.json(person)
     } else {
@@ -88,7 +47,6 @@ app.get('/api/persons/:id', (request, response, next) => {
 })
 
 app.get('/api/info', (request, response, next) => {
-  // const person_amount = persons.length
   const person_amount = Person.find({})
     .then(persons => {
       const person_amount = persons.length;
@@ -98,18 +56,9 @@ app.get('/api/info', (request, response, next) => {
       response.send(responsestring1 + responsestring2);
     })
     .catch(error => next(error))
-  // const time_now = new Date()
-  // const responsestring1 = '<h3>Phonebook has info for ' + person_amount + ' people'
-  // const responsestring2 = '<h3>' + time_now + '</h3>'
-  // response.send(responsestring1 + responsestring2)
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-//   const id = Number(request.params.id)
-//   persons = persons.filter(person => person.id !== id)
-
-//   response.status(204).end()
-// })
   Person.findByIdAndDelete(request.params.id)
   .then(result => {
     response.status(204).end()
@@ -134,51 +83,6 @@ app.put('/api/persons/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 })
-
-// const generateId = () => {
-//   const minvalue = 0
-//   const maxvalue = 10000
-//   const min = Math.ceil(minvalue)
-//   const max = Math.floor(maxvalue)
-//   const randomizedID = Math.floor(Math.random() * (max - min + 1)) + min
-//   return randomizedID
-// }
-
-const isAlready = (searchName) => {
-  const isFound = persons.some(person => person.name === searchName)
-  return isFound
-}
-
-
-// app.post('/api/persons', (request, response) => {
-//   const body = request.body
-
-//   if (!body.name) {
-//     return response.status(400).json({ 
-//       error: 'name missing' 
-//     })
-//   }
-//   else if (!body.number) {
-//     return response.status(400).json({ 
-//       error: 'number missing' 
-//     })
-//   }
-//   else if (isAlready(body.name)) {
-//     return response.status(400).json({ 
-//       error: 'name must be unique' 
-//     })
-//   }
-
-//   const person = {
-//     name: body.name,
-//     number: body.number,
-//     id: generateId(),
-//   }
-
-//   persons = persons.concat(person)
-
-//   response.json(person)
-// })
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
