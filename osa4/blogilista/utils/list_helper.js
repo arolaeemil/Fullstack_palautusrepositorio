c6d7a60b594a8347113d83c27375c1b20e021648
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-03-05 18:35:29 +0200

    t4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,8 +1,18 @@
 const dummy = (blogs) => {
     // ...
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+    let totalCount = 0
+    for (let obj of blogs) {
+        totalCount += obj.likes;
+    }
+    return totalCount
+}
   
 module.exports = {
-dummy
+dummy,
+totalLikes
 }
+
