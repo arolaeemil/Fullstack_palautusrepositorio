fc33acbbdd34b811d60c507b458e38ab8cf042cb
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-03-05 14:42:16 +0200

    osa4a_aloitus

---
+++ osa4/part4/index.js
@@ -0,0 +1,11 @@
+
+require('dotenv').config()
+
+// nämä lisättiin
+const app = require('./app') // varsinainen Express-sovellus
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
