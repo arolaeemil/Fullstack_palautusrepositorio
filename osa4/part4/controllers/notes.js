5d16c0e78a655afb926f2b6d759ec167662c28cc
arolaeemil <101641321+arolaeemil@users.noreply.github.com>
2024-07-11 10:22:13 +0300

    4c valmis

--- osa4/part4/controllers/notes.js
+++ osa4/part4/controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 /* notesRouter.get('/', (request, response) => {
   Note.find({}).then(notes => {
@@ -8,7 +9,9 @@ const Note = require('../models/note')
 }) */
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  //const notes = await Note.find({})
+  const notes = await Note
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
@@ -83,12 +86,18 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 })
 
